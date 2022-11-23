3a70d7359cdd5baad64805da0bd17ce03a5799d4
taoufiq lotfi <leh.lotfi@gmail.com>
2022-11-23 12:28:43 +0100

    added a better slug

--- schemas/documents/category.js
+++ schemas/documents/category.js
@@ -19,13 +19,30 @@ export default {
       options: {
         //Change to schema title to automatically populate
         source: 'title',
-        slugify: (input) =>
-          input
-            .toLowerCase()
-            .replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '')
+        slugify: (str) => {
+          str = str.replace(/^\s+|\s+$/g, ''); // trim
+          str = str.toLowerCase();
+
+          // remove accents, swap ñ for n, etc
+          var from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
+          var to = 'aaaaeeeeiiiioooouuuunc------';
+          for (var i = 0, l = from.length; i < l; i++) {
+            str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+          }
+
+          str = str
+            .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
             .trim()
-            //Remove spaces
-            .replace(/\s+/g, '-'),
+            .replace('-', '')
+            .replace(/\s+/g, '-') // collapse whitespace and replace by -
+            .replace(/-+/g, '-'); // collapse dashes
+
+          if (str.endsWith('-')) {
+            str = str.substring(0, str.length - 1);
+          }
+
+          return str;
+        },
         //Remove special characters
 
         validation: (Rule) => Rule.required(),
